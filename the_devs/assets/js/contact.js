230b6fc97ebdac33595568e687789ee41056ae5d
stinger13 <93597591+stinger13@users.noreply.github.com>
2022-05-07 19:40:21 +0530

    index.html connect

---
+++ the_devs/assets/js/contact.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form");
+let captcha = document.getElementById("captcha");
+const button = document.querySelector("button");
+
+button.disabled = true;
+
+form.addEventListener("keyup", () => {
+  let validity = form.checkValidity();
+  if (validity === true && captcha.value == 4) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+});
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  form.innerHTML =
+    "<div class='confirmation'>Hi there! This form is currently not connected to any mailscript.</div>";
+});
+
+function checkTab(e) {
+  if (e.keyCode === 9) {
+    button.classList.add("show-outline");
+    window.removeEventListener("keydown", checkTab);
+  }
+}
+window.addEventListener("keydown", checkTab);
